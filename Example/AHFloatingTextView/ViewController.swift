bc2495b605341af539b616f435f10d577e646f68
iOSModularization <>
2017-10-13 02:57:55 -0700

    improvements

--- Example/AHFloatingTextView/ViewController.swift
+++ Example/AHFloatingTextView/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import AHFloatingTextView
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let floatingView = AHFloatingTextView()
+        floatingView.text = "There's place in your heart and I know it is love!"
+        floatingView.frame = CGRect(x: 50, y: 20.0, width: 300, height: 50)
+        floatingView.backgroundColor = UIColor.black
+        floatingView.color = UIColor.white
+        floatingView.pausingInterval = 1.0
+        self.view.addSubview(floatingView)
     }
 
     override func didReceiveMemoryWarning() {
